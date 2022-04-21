f5cf685f3cf70b31949d2d7738934636a03c85ea
avetik.sukiasyan <avetik.sukiasyan@mati.io>
2022-04-21 16:31:17 +0400

    corrected identity Id key

--- lib/mati_plugin_flutter.dart
+++ lib/mati_plugin_flutter.dart
@@ -42,8 +42,8 @@ abstract class Result {
 
 class ResultSuccess extends Result {
   final String verificationId;
-  final String identittyId;
-  ResultSuccess(this.verificationId, this.identittyId);
+  final String identityId;
+  ResultSuccess(this.verificationId, this.identityId);
 }
 
 class ResultCancelled extends Result {
